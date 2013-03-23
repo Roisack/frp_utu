c3252c71b8d3e69ceda6c44767c0cdfcbbff9149
Mats Rauhala <mats.rauhala@iki.fi>
2013-03-23 23:17:38 +0200

    Parse students

--- code/Main.hs
+++ code/Main.hs
@@ -10,12 +10,36 @@ import qualified Data.Vector as V
 import qualified System.IO as IO
 import Control.Applicative
 import Control.Monad
-import Data.Maybe (maybe)
+import Data.Maybe (maybe, catMaybes)
 import Data.List (groupBy)
 import Data.Function (on)
 
-type Course = Text
-data Thesis = Thesis Text [Course] deriving Show
+type Course        = Text
+type Name          = Text
+type StudentPoints = Int
+type StudentID     = Text
+type Major         = Text
+type Degree        = Text
+type Year          = Int
+data Thesis        = Thesis Name [Course] deriving Show
+data Date          = Date Year Season deriving Show
+data Season        = Autumn | Spring deriving Show
+data Student       = Student Date StudentID Name StudentPoints Degree Major deriving Show
+
+parseStudents :: FilePath -> IO [Student]
+parseStudents path = do
+  catMaybes . map parseStudent . map (T.splitOn ";") .  T.splitOn "\r\n" <$> TI.readFile path
+  where
+    parseStudent (date : studentId : name : points : degree : major : _ ) = do
+      let (date', season) = T.splitAt 4 date
+      points' <- readMay (T.unpack points)
+      date'' <- readMay $ T.unpack date'
+      season' <- case T.unpack season of
+                      "S" -> return Autumn
+                      "K" -> return Spring
+                      _ -> Nothing
+      return $ Student (Date date'' season') studentId name points' degree major
+    parseStudent _ = Nothing
 
 parseThesis :: FilePath -> IO [Thesis]
 parseThesis path = do
